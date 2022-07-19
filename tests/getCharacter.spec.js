17c49f63cbc495e252b28a6ae2c7c73a0b5b3b08
Felipe <luizbrutante@gmail.com>
2022-07-19 19:05:21 -0300

    Requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -42,8 +42,37 @@ Retorno:
 */
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
-  it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+  it('Verifica se a função, quando não recebe nenhum parâmetro, retorna undefined', () => {
+    expect(getCharacter()).toBeUndefined();
+  });
+  it('Verifica se a função retorna o objeto correto para o parâmetro "Arya"', () => {
+    expect(getCharacter('Arya')).toEqual({
+      name: 'Arya Stark',
+      class: 'Rogue',
+      phrases: [ 'Not today', 'A girl has no name.' ]
+    });
+  });
+  it('Verifica se a função retorna o objeto correto para o parâmetro "Brienne"', () => {
+    expect(getCharacter('Brienne')).toEqual({
+      name: 'Brienne Tarth',
+      class: 'Knight',
+      phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
+    });
+  });
+  it('Verifica se a função retorna o objeto correto para o parâmetro "Melissandre"', () => {
+    expect(getCharacter('Melissandre')).toEqual({
+      name: 'Melissandre',
+      class: 'Necromancer',
+      phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
+    });
+  });
+  it('Verifica se  parâmetro não é Case Sensitive', () => {
+    expect (getCharacter('Brienne')).toEqual(getCharacter('Brienne'));
+  });
+  it('Verifica se ao passar um nome que não está na tabela, a função retorna undefined', () => {
+    expect (getCharacter('Tyrion')).toBeUndefined();
+  });
+});
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
@@ -51,5 +80,4 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
-  });
-});
+
